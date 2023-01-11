e3c418594decc4910ee03fb3785ab65037263a1f
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-11 23:13:26 +0500

    Первый коммит

---
+++ src/main/java/com/example/budgetapp/BookOfRecipes.java
@@ -0,0 +1,13 @@
+package com.example.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfRecipes {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookOfRecipes.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/budgetapp/FirstController.java
@@ -0,0 +1,11 @@
+package com.example.budgetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+}
