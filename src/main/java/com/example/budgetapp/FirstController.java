fbdf73ea50b3dc21702e66bf5d21807f2051e49d
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-11 23:40:47 +0500

    Сделано

--- src/main/java/com/example/budgetapp/FirstController.java
+++ src/main/java/com/example/budgetapp/FirstController.java
@@ -7,5 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping
+    public String appGet() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String page() {
+        return  "Имя ученика: Адигамова Кристина<br><br>Название проекта: Книга рецептов"  +
+                "\nДата создание: 11.01.2023\n" +
+                "\nОписание: \n" +
+                "\nНазвание проекта: Книга рецептов\n" +
+                "\nФункции проекта: Хранить и выводить информацию разных рецептов со всего мира\n" +
+                "\nТехнологии, которые используются: Maven & Gradle\n" +
+                "\nЯзык проекта: Java";
+    }
 
 }
